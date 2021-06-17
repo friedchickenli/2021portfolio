12117262a6a9b8c0131ac784f7b578870f898700
friedchickenli <mzli1001@uw.edu>
2021-06-17 14:32:04 -0700

    tried starting on work page

--- pages/work.vue
+++ pages/work.vue
@@ -1,3 +1,22 @@
 <template>
-  
+  <div id="workContainer">
+        <!-- <span class="titleWithOutline" :data-text="workContent.work_intro_text">
+          {{workContent.intro_text}}
+        </span>
+        <prismic-rich-text class="pText" :field="workContent.work_intro_description" /> -->
+  </div>
 </template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  data() {
+    return{
+      workContent: this.$store.state.work,
+    }
+  }
+}
+</script>
